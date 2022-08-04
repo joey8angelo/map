4fca029704a7b7a430c38a2969c5028765af4e67
joey8angelo <joey8angelo@gmail.com>
2022-08-04 00:13:43 -0700

    minor testcase

--- main.cpp
+++ main.cpp
@@ -10,7 +10,8 @@ int main() {
     mp1.insert(std::pair<int, int>(5, 7));
     mp1.insert(std::pair<int, int>(7, 8));
 
-    mp1[2] = 3;
+    mp1[2];
+    mp1.find(2).second() = 3;
     mp1[5] = 6;
 
     for (iterator<int, int> b = mp1.begin(); b != mp1.end(); ++b) {
